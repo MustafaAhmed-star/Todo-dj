84628281dcf924f1728135d964e873c535d483d8
Mustafa  Abdelmgeed <mostafa012155@gmail.com>
2024-09-04 00:51:05 +0300

    init : model &template&VIEW

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Task
 
-# Create your views here.
+
+def index(requeset):
+
+    return HttpResponse("Hello, World!")
+
+def taskList(request):
+    tasks = Task.objects.all()
+    
+    context = {'tasks':tasks}
+    
+    return render(request,'tasks.html',context)

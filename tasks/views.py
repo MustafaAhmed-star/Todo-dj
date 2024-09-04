2ba81eac1c08d3593e09d05bc22976fe17e2a9f8
Mustafa  Abdelmgeed <mostafa012155@gmail.com>
2024-09-04 23:56:53 +0300

    "Updated tasks.html template, added form to taskList view in views.py, and modified db.sqlite3 and __pycache__/views.cpython-312.pyc files"

--- tasks/views.py
+++ tasks/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render, HttpResponse
 from .models import Task
-
+from .forms import TaskForm
 
 def index(requeset):
 
     return HttpResponse("Hello, World!")
 
 def taskList(request):
+    form = TaskForm()
+    
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
     tasks = Task.objects.all()
     
-    context = {'tasks':tasks}
+    context = {'tasks':tasks,'form':form}
     
     return render(request,'tasks.html',context)

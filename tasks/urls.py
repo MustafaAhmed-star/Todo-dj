9968ddd74d55fb87316767e2568f53bc885d9ee6
Mustafa  Abdelmgeed <mostafa012155@gmail.com>
2024-09-05 00:32:00 +0300

    "Updated .gitignore, added KoloMiddleware to settings, modified tasks templates and views, and added updateTask view and URL"

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('tasks/',views.taskList,name='tasks'),
+    path('tasks/<str:pk>/update/',views.updateTask,name='update-task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse,redirect
 from .models import Task
 from .forms import TaskForm
 
@@ -18,3 +18,19 @@ def taskList(request):
     context = {'tasks':tasks,'form':form}
     
     return render(request,'tasks.html',context)
+    
+    
+def updateTask(request,pk):
+    task = Task.objects.get(id = pk)
+    form = TaskForm(instance =task)
+    
+    if request.method == 'POST':
+        form = TaskForm(request.POST,instance = task)
+        if form.is_valid():
+            form.save()
+            return redirect('/tasks/')
+            
+    context = {'task':task,'form':form}
+    
+    return render(request,'update_task.html',context)
+    
